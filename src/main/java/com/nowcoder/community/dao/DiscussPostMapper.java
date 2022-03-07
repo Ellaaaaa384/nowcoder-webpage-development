b2f2ea7d5f242983436aeb07632451b633afcbe7
Ellaaaaa384 <shiyuan384@163.com>
2022-03-07 14:40:53 +0800

    DiscussPost

--- src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
+++ src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
@@ -9,6 +9,7 @@ import java.util.List;
 @Mapper
 public interface DiscussPostMapper {
 
+    // 查询有关
     List<DiscussPost> selectDiscussPosts(int userId, int offset, int limit);
     // userId用于未来个人主页发帖记录显示，offset用于分页起始行，limit表示每页最多行数
 
@@ -16,4 +17,9 @@ public interface DiscussPostMapper {
     // 如果只有一个参数，且在<if>中使用（动态拼接sql），必须取别名（也可以写和原名一样的名）
     int selectDiscussPostRows(@Param("userId") int userId);
 
+    // 增加帖子
+    int insertDiscussPost(DiscussPost discussPost);
+
+    // 查询帖子（根据id查询详细信息）
+    DiscussPost selectDiscussPostById(int id);
 }
