84b85e9c7362ba997b02e5e6da1a5772896840c1
Ellaaaaa384 <shiyuan384@163.com>
2022-03-28 20:20:10 +0800

    redis example 2

--- src/test/java/com/nowcoder/community/RedisTests.java
+++ src/test/java/com/nowcoder/community/RedisTests.java
@@ -3,7 +3,11 @@ package com.nowcoder.community;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.core.BoundValueOperations;
+import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SessionCallback;
 import org.springframework.test.context.ContextConfiguration;
 
 import java.util.concurrent.TimeUnit;
@@ -83,6 +87,42 @@ public class RedisTests {
         redisTemplate.delete("test:user");
         System.out.println(redisTemplate.hasKey("test:user"));
         redisTemplate.expire("test:students", 10, TimeUnit.SECONDS);
+    }
+
+    // 多次访问同一个key
+    @Test
+    public void testBoundOperations() {
+        String redisKey = "test:count";
+        BoundValueOperations operations = redisTemplate.boundValueOps(redisKey);
+        operations.increment();
+        operations.increment();
+        operations.increment();
+        operations.increment();
+        operations.increment();
+        System.out.println(operations.get());
+    }
+
+    // Redis事务管理（不严格满足ACID四个特性，因为不是关系型数据库）
+    // 待执行命令放在队列里，提交事务时把命令全部发给Redis服务器一起执行
+    // 所以事务中间的查询不会立即返回，要么开头要么结尾查
+    // 所以通常使用「编程式事务」把范围缩小，「声明式事务」用得少
+    @Test
+    public void testTransactional() {
+        Object obj = redisTemplate.execute(new SessionCallback() {
+            @Override
+            public Object execute(RedisOperations operations) throws DataAccessException {
+                String redisKey = "test:tx";
+                operations.multi(); // 启用事务
+                operations.opsForSet().add(redisKey,"zhangsan");
+                operations.opsForSet().add(redisKey,"lisi");
+                operations.opsForSet().add(redisKey,"wangwu");
+
+                System.out.println(operations.opsForSet().members(redisKey)); // 无效（此时不会执行）
+
+                return operations.exec(); // 提交事务
+            }
+        });
+        System.out.println(obj); // 结果中的1代表每个命令影响的数据的个数
     }
 
 }
